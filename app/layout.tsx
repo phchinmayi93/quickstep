e8d36d5957670fd77cf44e0017537dd88f2ae0aa
chinmayi <phchinmayi93@gmail.com>
2026-03-18 10:02:47 +0530

    added seo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,12 @@ import "./globals.css";
 import Topbar from "@/components/Topbar";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import ContactInfoSection from "@/components/ContactInfoSection";
+
+export const metadata = {
+  title: "QuickStep Computer Center | All Courses One Institute",
+  description:
+    "Best training institute in R.T Nagar Bangaluru We are into Training & Placements. Civil-Architectural CAD, Mechanical CAD, Electrical CAD, Interior Design, SAP,HANA,SuccessFactors,NLP, Python, R-Programming , Machine Learning, Artificial Intelligence, Web Development, Data Science, Data Analytics, DEV-OPS,C,C++,Java-J2EE,.NET,MVC,Software Testing,Selenium,RPA,H.R Training,IT Professionals , Tally Erp 9 GST, Advance Excel, Mis, Spoken English, Foreign Languages, IELTS, Etc Many more.",
+};
 
 export default function RootLayout({
   children,
@@ -15,8 +20,7 @@ export default function RootLayout({
         <Topbar />
         <Navbar />
         {children}
-      
-       <Footer/>
+        <Footer />
       </body>
     </html>
   );
